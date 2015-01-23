88e5d7a63f6054f91a2209ba0774da05890c4875
jschroder <joshua.schroder@dshs.state.tx.us>
2015-01-23 16:09:40 +0000

    Daily Backup for Fri Jan 23 16:09:40 UTC 2015

--- includes/keepalive.sh
+++ includes/keepalive.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-HOUR=$(date +%H)
-
-while [ $HOUR -lt 23 ]
+GMT=$(date +%H)
+MOD=6
+HOUR=$(($GMT - $MOD))
+echo $HOUR
+while [ $HOUR -lt 18 ]
     do echo  "The number of seconds elapsed since 01/01/1970 is `date +%s`."
     sleep 84
     done
--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@ parts stop apache2
 parts start apache2
 parts start mysql
 git commit -m "Daily Backup for $(date)" -a
+git push origin master
 ./includes/keepalive.sh &
