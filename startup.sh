138720c41cb5fb7372d0ceb7dde1531df950c136
jschroder <joshua.schroder@dshs.state.tx.us>
2015-08-26 18:06:16 +0000

    Daily Backup for Wed Aug 26 18:06:16 UTC 2015

--- startup.sh
+++
@@ -1,9 +0,0 @@
-parts stop apache2
-parts start apache2
-#parts stop fcron
-#parts start fcron
-parts start mysql
-git add .
-git commit -m "Daily Backup for $(date)" -a
-git push origin master
-./includes/keepalive.sh &
