49afa26d369469963c0990bdf0af0b14d5443adf
jschroder <joshua.schroder@dshs.state.tx.us>
2015-11-30 18:33:28 +0000

    Daily Backup for Mon Nov 30 18:33:28 UTC 2015

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ parts stop apache2
 parts start apache2
 #parts stop fcron
 #parts start fcron
-parts start mysql
+#parts start mysql
 git add .
 git commit -m "Daily Backup for $(date)" -a
 git push origin master
